ee2df3c99b6bf18b44deebbe95f8302f771618b9
Dmitry Uvarov <dm.a.uvarov@gmail.com>
2021-12-23 03:52:22 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+
+// 1#
+function introduction(name) {
+    return `Hi, my name is ${name}.`
+} 
+
+// 2#
+function introductionWithLanguage(name, langauge) {
+    return `Hi, my name is ${name} and I am learning to program in ${langauge}.`
+} 
+
+// 3#
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+
+
